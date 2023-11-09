88bd115b8cba50d4d8bbe87bf9318085347f245c
Jasmin Flores <jasminf2202@gmail.com>
2023-11-09 18:52:09 -0500

    added refresh functions

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ $(function () {
   var currentTime = dayjs().hour();
 
   
-
+//color code
   var timeBlockList = $('.time-block')
 
   for (var i = 0; i<timeBlockList.length;i++){
@@ -20,6 +20,7 @@ $(function () {
   }
   }
   
+  //save user input
 
   $('.saveBtn').on('click', function (){
     var hour = $(this).siblings('.hour').text();
@@ -28,15 +29,31 @@ $(function () {
       localStorage.setItem(hour,descriptionBlock);
   });
 
-
+  // refresh page 
+  var userRefresh = function () {
+    $('.hour').each(function () {
+        if ($(this).text() !== null) {
+            $(this).siblings('.description').val(localStorage.getItem($(this).text()));
+        }
+    });
+};
+// refresh page every 30 minutes
+setInterval(function () {
+  $('.hour').each(function () {
+      reviewTask();
+  });
+}, 1800000);
   
 
-
+//display time
 
   
   fullDate = dayjs().format('dddd'+' '+'MMMM'+' '+ 'D')
   var timeDisplayEl = $('#currentDay');
   timeDisplayEl.text( fullDate);
+
+  userRefresh();
+  reviewTask()
 
 
 });
